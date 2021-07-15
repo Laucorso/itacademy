4fdfc30d03dbffe8f693e1571f98671660b6742a
LauraCormand <laucorrs@gmail.com>
2021-07-15 17:16:26 +0200

    Tasca 5 nivell 1 complet

---
+++ tascaM5/exercici1.php
@@ -0,0 +1,42 @@
+<?php
+
+$edat = 45;
+$notamitjana = 8,752;
+$nom = Marta;
+$aprovat = true;
+echo "Variable int: ";
+echo $edat;
+echo "Variable double: ";
+echo $notamitjana;
+echo "Variable string: ";
+echo $nom;
+echo "Variable boolean: ";
+echo $aprovat;
+
+- Exercici 1
+
+
+
+Defineix una variable de cada tipus: integer,double,string i boolean. Després, imprimeix-les per pantalla.
+
+
+- Exercici 2
+
+
+
+
+Crea una altra variable string. Després:
+
+Imprimeix per pantalla el tamany(longitud) del dos strings.
+Imprimeix per pantalla el dos strings però en ordre invers de caràcters.
+Imprimeix la concatenació dels dos strings. 
+- Exercici 3
+
+
+
+
+Crea una constant que inclogui el teu nom i imprimeix-la per pantalla. 
+
+
+
+?>
