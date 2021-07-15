3a797f0bcfff13396a19cabcae61ece72a0ac42d
LauraCormand <laucorrs@gmail.com>
2021-07-15 17:16:26 +0200

    Tasca 5 nivell 1 complet

---
+++ tascaM5/index.php
@@ -0,0 +1,49 @@
+
+<?php
+
+/*EXERCICI 1*/
+echo "<b>Exercici 1</b>";
+echo "<br>";
+$edat = 18;
+$double = 8.75;
+$nom = "Marta";
+$aprovada = true;
+
+echo $nom;
+echo "<br>";
+echo $edat;
+echo "<br>";
+echo $double;
+echo "<br>";
+echo $aprovada;
+echo"<br>";
+echo"<br>";
+
+/*EXERCICI 2*/
+echo "<b>Exercici 2</b>";
+echo "<br>";
+$cognom = " Garcia";
+$tot = $nom.$cognom;
+
+echo strlen($tot);
+echo "<br>";
+echo strrev($tot);
+echo "<br>";
+echo $tot;
+echo"<br>";
+echo"<br>";
+
+/*EXERCICI 3*/
+echo "<b>Exercici 3</b>";
+echo"<br>";
+define("elMeuNom", "Laura Cormand Solà");
+echo elMeuNom;
+echo"<br>";
+
+
+
+
+        
+?>
+
+
