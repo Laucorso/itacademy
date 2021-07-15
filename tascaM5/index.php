d36352de1aaa3d7c95f028a20a9d458c26654254
LauraCormand <laucorrs@gmail.com>
2021-07-15 17:47:51 +0200

    Nivell 2 complet

--- tascaM5/index.php
+++ tascaM5/index.php
@@ -1,8 +1,8 @@
 
 <?php
 
-/*EXERCICI 1*/
-echo "<b>Exercici 1</b>";
+/* Nivell 1 EXERCICI 1*/
+echo "<b> Nivell 1 Exercici 1</b>";
 echo "<br>";
 $edat = 18;
 $double = 8.75;
@@ -39,11 +39,29 @@ echo"<br>";
 define("elMeuNom", "Laura Cormand Solà");
 echo elMeuNom;
 echo"<br>";
+echo"<br>";
 
+/*Nivell 2 EXERCICI 1*/
+echo "<b>Nivell 2 Exercici 1</b>";
+echo"<br>";
+$notes1 = array(8, 9, 4, 3, 8);
+$notes2 = array(3, 9, 9);
+echo"<br>";
+echo"<br>";
 
+/*Nivell 2 EXERCICI 2*/
+echo "<b>Exercici 2</b>";
+echo"<br>";
+$notes2 [3] = 5;
+echo"<br>";
+echo"<br>";
 
+/*Nivell 2 EXERCICI 3*/
+echo "<b>Exercici 3</b>";
+echo"<br>";
+$array_resultant = array_merge($notes1,$notes2);
+echo count($array_resultant);
 
         
 ?>
-
 
