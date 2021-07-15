2a19f60401da4a108baf69ebe826f02df616af7d
LauraCormand <laucorrs@gmail.com>
2021-07-15 18:44:17 +0200

    Nivell 3 complet

--- tascaM5/index.php
+++ tascaM5/index.php
@@ -61,7 +61,64 @@ echo "<b>Exercici 3</b>";
 echo"<br>";
 $array_resultant = array_merge($notes1,$notes2);
 echo count($array_resultant);
+echo"<br>";
+echo"<br>";
+/*Nivell 3 EXERCICI 1*/
+echo "<b>Nivell 3 Exercici 1</b>";
+echo"<br>";
+echo "Les notes són les següents:  " . $array_resultant[0] . ", " . $array_resultant[1] . ", " . $array_resultant[2] . ", " . $array_resultant[3] . ", " . $array_resultant[4] . ", " . $array_resultant[5] . ", " . $array_resultant[6] . ", " . $array_resultant[7] . ", " . $array_resultant[8] . ".";
+echo"<br>";
+echo"<br>";
+/*Nivell 3 EXERCICI 2*/
+echo "<b>Exercici 2</b>";
+echo"<br>";
+$x = 3;
+$y = 10;
+$n = 5.56;
+$m = 1.99;
+echo ($x + $y);
+echo"<br>";
+echo ($x - $y);
+echo"<br>";
+echo ($x * $y);
+echo"<br>";
+echo ($x/$y);
+echo"<br>";
+echo ($x % $y);
+echo"<br>";
+echo ($x **2);
+echo"<br>";
+echo ($x **2);
+echo"<br>";
+echo ($y **2);
+echo"<br>";
+echo ($n **2);
+echo"<br>";
+echo ($m **2);
+echo"<br>";
+echo ($n+$m+$y+$x);
+echo"<br>";
+echo ($n*$m*$y*$x);
 
+/*- Exercici 2
+
+Escriure un programa PHP que realitzi lo següent: 
+
+declarar dues variables X e Y de tipus int, dues variables N i M de tipous double i assigna a cada una un valor. A continuació, mostra per pantalla, per a X e Y:
+
+El valor de cada variable
+La suma 
+La resta 
+El producte  
+La divisió 
+El mòdul
+per a N i M, lo mateix.
+
+I per a totes les variables(X,Y,N,M):
+
+El doble de cada variable
+La suma de totes les variables
+El producte de totes les variables*/
         
 ?>
 
