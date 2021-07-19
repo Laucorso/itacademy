73e4b49786459caa13c457ad6832949874b5cda2
LauraCormand <laucorrs@gmail.com>
2021-07-19 19:12:42 +0200

    Nivell 1 amb bon format

--- tascaM6/index.php
+++ tascaM6/index.php
@@ -13,30 +13,40 @@
     function resta ($x, $y){
         return $x-$y;
     }
+    echo "<b> Exercici 1: </b><br>";
     echo resta (10,9);
+    echo "<br>";
     
     /*exercici 2*/
     $num = 15;
     if ($num%2==0){
+        echo "<b> Exercici 2: </b><br>";
         echo "el $num és parell";
     }else{
+        echo "<b> Exercici 2: </b><br>";
         echo "el $num és imparell";
     }
+    echo "<br>";
 
     /*exercici 3 */
     function parell_o_imparell($num){
         if ($num%2==0){
+            echo "<b> Exercici 3: </b><br>";
             echo "el $num és parell";
         }else{
+            echo "<b> Exercici 3: </b><br>";
             echo "el $num és imparell";
         }
     }
     parell_o_imparell($num);
-    
+    echo "<br>";
+
     /*exercici 4*/
+    echo "<b> Exercici 4: </b><br>";
     for($i = 0; $i<=10;$i++){
         echo $i . " ";
     }
+    echo "<br>";
 
 
     ?>
