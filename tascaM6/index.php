24c68161e687dab00006d8c7a7d918efab384096
LauraCormand <laucorrs@gmail.com>
2021-07-21 18:25:23 +0200

    Nivell 2 complet

--- tascaM6/index.php
+++ tascaM6/index.php
@@ -48,6 +48,41 @@
     }
     echo "<br>";
 
+    /* Nivell 2 - Exercici 1*/
+
+    echo "<b> Nivell 2 Exercici 1: </b><br>";
+    function amagatall(){
+        for($i = 0; $i<=10; $i++){
+            echo $i;
+            $i++;
+        }
+    }
+    amagatall();
+    echo "<br>";
+
+    echo "<b> Nivell 2 Exercici 2: </b><br>";
+    $na = mt_rand(1,100);
+    function comptar($na){
+        for($i= 0; $i<=$na; $i++)
+        echo $i . " ";
+    }
+    comptar($na);
+    echo "<br>";
+
+    echo "<b> Nivell 2 Exercici 3: </b><br>";
+    $numero = 10;
+    function amagatall2($numero){
+        for($i=0; $i<=$numero; $i++){
+            echo $i . " ";
+        }
+    }
+    amagatall2($numero);
+
+/*
+- Exercici 3
+Per a prevenir oblits al utilitzar la nostra meravellosa funció "amagatall" 
+establirem un paràmetre per defecte a 10 en la funció que s'encarrega de fer aquest compte. */
+
 
     ?>
 </body>
