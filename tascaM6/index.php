aac6ad55879b3086316f28d65dd26791a444a4b9
LauraCormand <laucorrs@gmail.com>
2021-07-21 19:40:18 +0200

    Nivell 3 exercici 1 complet

--- tascaM6/index.php
+++ tascaM6/index.php
@@ -77,11 +77,41 @@
         }
     }
     amagatall2($numero);
+    echo "<br>";
+
+    echo "<b> Nivell 3 Exercici 1: </b><br>";
+    for($i=1960; $i<=2016; $i++){
+
+        echo $i . " ";
+        $i++;
+        $i++;
+        $i++;
+    }
+
 
 /*
+- Exercici 1
+
+Ens han demanat un llistat de tots els anys on es van produir jocs olímpics desde 1960 inclós fins al 2016. Programa un bucle que calculi i mostri per pantalla els anys olímpics dins de l'interval esmentat.
+
+- Exercici 2
+
+Imagina que som a una botiga on es ven:
+
+Xocolata: 1 euro
+Xiclets: 0.50 euros
+Carmels: 1.50 euros
+Implementa un programa que permeti afegir càlculs a un total de compres d'aquest tipus. Per exemple, que si comprem:
+
+2 xocolates, 1 de xiclets i 1 de carmels, tinguem un programa que permeti anar afegint els subtotals a un total, tal que així,
+
+funció(2 xocolates) + funció(1 de xiclets) + funció(1 de carmels) = 2+0.5+1.5
+
+Sent per tant el total, 4.
+
 - Exercici 3
-Per a prevenir oblits al utilitzar la nostra meravellosa funció "amagatall" 
-establirem un paràmetre per defecte a 10 en la funció que s'encarrega de fer aquest compte. */
+
+La criba d'Eratóstenes és un algoritme pensat per a trobar nombres primers dins d'un interval donat. Basats en l'informació de l'enllaç adjunt, implementa la criba d'Eratóstenes dins d'una funció, de tal forma que poguem invocar la funció per a un número concret. */
 
 
     ?>
